4504e15b0ef188fdfbfecf5529a72ff217788e70
rudi <rudiirnawansaputra@gmail.com>
2025-05-20 21:32:54 +0700

    add task

--- Program1.java
+++ Program1.java
@@ -1,22 +1,5 @@
-public class Program1 {
+public class program1 {
     public static void main(String[] args) {
-        // Tipe data boolean
-        boolean b = true;
-        System.out.println(b); 
-
-        boolean c = false;
-        System.out.println(c); 
-
-        int a = 3 + 4;
-        System.out.println(a); 
-
-        double d = Math.random();
-        System.out.println(d); 
-
-        char e = 'd' + 1;
-        System.out.println(e); 
-        
-        String h = "Hello!";
-        System.out.println(h); 
+        System.out.println("Hello world!");
     }
 }
